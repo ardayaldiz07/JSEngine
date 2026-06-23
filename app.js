a555e60608c2fbd21cc00879fa4630ed698b5095
ardayaldiz07 <ardayaldiz07@gmail.com>
2026-06-24 00:48:23 +0300

    physics02

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import JsEngine from "./core/JSEngine.js";
 import GameObject from "./core/GameObject.js";
  
 class Player extends GameObject{
-    constructor(name,x,y,w,h,c,useGravity) {
-        super(name,x,y,w,h,c,useGravity);
+    constructor(name,x,y,w,h,c,useGravity,useCollision,isStatic) {
+        super(name,x,y,w,h,c,useGravity,useCollision,isStatic);
         
     }
  
@@ -17,8 +17,8 @@ class Player extends GameObject{
 };
 
 class Ground extends GameObject{
-    constructor(name,x,y,w,h,c,useGravity) {
-        super(name,x,y,w,h,c,useGravity);
+    constructor(name,x,y,w,h,c,useGravity,useCollision,isStatic) {
+        super(name,x,y,w,h,c,useGravity,useCollision,isStatic);
         
     }
  
@@ -39,8 +39,8 @@ document.addEventListener('DOMContentLoaded',()=>{
         bgColor:"green"
     }
     const Engine = new JsEngine(options);
-    const player = new Player("player",0,0,32,32,"blue",true);
-    const ground = new Ground("ground",0,200,1000,1000,"red",false);
+    const player = new Player("player",0,0,32,32,"blue",true,true,false);
+    const ground = new Ground("ground",0,200,1000,1000,"red",false,true,true);
     Engine.addGameObject(player);
     Engine.addGameObject(ground);
 });
--- core/GameObject.js
+++ core/GameObject.js
@@ -1,5 +1,5 @@
 class GameObject {
-    constructor(name, x, y, w, h, c, useGravity = false,useCollision = false) {
+    constructor(name, x, y, w, h, c, useGravity = false, useCollision = false, isStatic = false, bounce = 0) {
         this.name = name;
         this.x = x;
         this.y = y;
@@ -9,8 +9,10 @@ class GameObject {
         this.useGravity = useGravity;
         this.velX = 0;
         this.velY = 0;
-        this.gravityValue = 0.5; 
+        this.gravityValue = 0.5;
         this.useCollision = useCollision;
+        this.isStatic = isStatic;
+        this.bounce = bounce;
     }
 
     update() {
@@ -31,7 +33,6 @@ class GameObject {
         this.velY += this.gravityValue;
     }
 
-    // AABB
     isColliding(other) {
         return (
             this.x < other.x + other.w &&
@@ -41,8 +42,8 @@ class GameObject {
         );
     }
 
-resolveCollision(other) {
-        if (!this.isColliding(other)) return;
+    resolveCollision(other) {
+        if (!this.isColliding(other) || (this.isStatic && other.isStatic)) return;
 
         const dx = (this.x + this.w / 2) - (other.x + other.w / 2);
         const dy = (this.y + this.h / 2) - (other.y + other.h / 2);
@@ -53,26 +54,32 @@ resolveCollision(other) {
         const overlapX = minDistX - Math.abs(dx);
         const overlapY = minDistY - Math.abs(dy);
 
+        const thisPushRatio = this.isStatic ? 0 : (other.isStatic ? 1 : 0.5);
+        const otherPushRatio = other.isStatic ? 0 : (this.isStatic ? 1 : 0.5);
+
         if (overlapX >= overlapY) {
             if (dy > 0) {
-                this.y += overlapY / 2; 
-                other.y -= overlapY / 2;
+                this.y += overlapY * thisPushRatio;
+                other.y -= overlapY * otherPushRatio;
             } else {
-                this.y -= overlapY / 2;
-                other.y += overlapY / 2;
+                this.y -= overlapY * thisPushRatio;
+                other.y += overlapY * otherPushRatio;
             }
-            this.velY = -this.velY;
-            other.velY = -other.velY;
+
+            if (!this.isStatic) this.velY *= -this.bounce;
+            if (!other.isStatic) other.velY *= -other.bounce;
+
         } else {
             if (dx > 0) {
-                this.x += overlapX / 2;
-                other.x -= overlapX / 2;
+                this.x += overlapX * thisPushRatio;
+                other.x -= overlapX * otherPushRatio;
             } else {
-                this.x -= overlapX / 2;
-                other.x += overlapX / 2;
+                this.x -= overlapX * thisPushRatio;
+                other.x += overlapX * otherPushRatio;
             }
-            this.velX = -this.velX;
-            other.velX = -other.velX;
+
+            if (!this.isStatic) this.velX *= -this.bounce;
+            if (!other.isStatic) other.velX *= -other.bounce;
         }
     }
 }
